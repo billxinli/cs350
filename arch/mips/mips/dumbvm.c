d0046c06c3392abf05aee1b840ad013ba926d7f3
Steven <sdmacleo@uwaterloo.ca>
2010-11-09 01:12:10 +0000

    fixed the warnings in the dumbvm

--- arch/mips/mips/dumbvm.c
+++ arch/mips/mips/dumbvm.c
@@ -316,25 +316,25 @@ as_copy(struct addrspace *old, struct addrspace **ret)
 
 #if OPT_A2
 int as_valid_read_addr(struct addrspace *as, vaddr_t *check_addr){
-    if(check_addr < USERTOP){
-        if((int) check_addr >= as->as_vbase1 && check_addr < (as->as_vbase1 + as->as_npages1 * PAGE_SIZE)){
+    if(check_addr < (vaddr_t*) USERTOP){
+        if(check_addr >= (vaddr_t*)as->as_vbase1 && check_addr < (vaddr_t*)(as->as_vbase1 + as->as_npages1 * PAGE_SIZE)){
             return 1;
         }
-        if(check_addr >= as->as_vbase2 && check_addr < (as->as_vbase2 + as->as_npages2 * PAGE_SIZE)){
+        if(check_addr >= (vaddr_t*)as->as_vbase2 && check_addr < (vaddr_t*)(as->as_vbase2 + as->as_npages2 * PAGE_SIZE)){
             return 1;
         }
-        if(check_addr >= (USERTOP - DUMBVM_STACKPAGES * PAGE_SIZE)){
+        if(check_addr >= (vaddr_t*)(USERTOP - DUMBVM_STACKPAGES * PAGE_SIZE)){
             return 1;
         }
     }
     return 0;
 }
 int as_valid_write_addr(struct addrspace *as, vaddr_t *check_addr){
-    if((int) check_addr < USERTOP){
-        if(check_addr >= as->as_vbase2 && check_addr < (as->as_vbase2 + as->as_npages2 * PAGE_SIZE)){
+    if(check_addr < (vaddr_t*)USERTOP){
+        if(check_addr >= (vaddr_t*)as->as_vbase2 && check_addr < (vaddr_t*)(as->as_vbase2 + as->as_npages2 * PAGE_SIZE)){
             return 1;
         }
-        if(check_addr >= (USERTOP - DUMBVM_STACKPAGES * PAGE_SIZE)){
+        if(check_addr >= (vaddr_t*)(USERTOP - DUMBVM_STACKPAGES * PAGE_SIZE)){
             return 1;
         }
     }

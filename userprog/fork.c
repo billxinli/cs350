327b5850de6e9420a908e62b45f8e85f82212deb
Matt <mtpharoa@uwaterloo.ca>
2010-11-09 03:34:35 +0000

    Fixed assertion fail in forkbomb

--- userprog/fork.c
+++ userprog/fork.c
@@ -20,12 +20,14 @@ pid_t sys_fork(struct trapframe *tf) {
     char *child_name = kmalloc(sizeof(char) * (strlen(curthread->t_name)+9));
     if (child_name == NULL) {
         //error
+        splx(spl);
         return ENOMEM;
     }
     child_name = strcpy(child_name, curthread->t_name);
     struct child_table *new_child = kmalloc(sizeof(struct child_table));
     if (new_child == NULL) {
         //error
+        splx(spl);
         return ENOMEM;
     }
     struct thread *child = NULL;
@@ -35,6 +37,7 @@ pid_t sys_fork(struct trapframe *tf) {
     if (result != 0) {
         kfree(new_child);
         //ERROR
+        splx(spl);
         return result;
     }  
     
@@ -54,6 +57,7 @@ pid_t sys_fork(struct trapframe *tf) {
         child->t_vmspace = NULL;
         child->parent = NULL; //to prevent thread_destroy from freeing a non-existant pid
         md_initpcb(&child->t_pcb, child->t_stack, 0, 0, thread_exit); //set new thread to delete itself
+        splx(spl);
         return err;
     }
     //now copy the file table
@@ -63,6 +67,7 @@ pid_t sys_fork(struct trapframe *tf) {
             DEBUG(DB_THREADS, "Not enough memory to copy file table in fork. Closing child...\n");
             child->parent = NULL; //to prevent thread_destroy from freeing a non-existant pid
             md_initpcb(&child->t_pcb, child->t_stack, 0, 0, thread_exit); //set new thread to delete itself
+            splx(spl);
             return ENOMEM;
         }
     }

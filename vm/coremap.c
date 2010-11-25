d70c602aff3a2bedbbc576122a2aff4c36c0ae49
Matt <mtpharoa@uwaterloo.ca>
2010-11-25 13:57:45 +0000

    Added some stuff to coremap

--- vm/coremap.c
+++ vm/coremap.c
@@ -3,15 +3,37 @@
 #if OPT_A3
 #include <thread.h>
 #include <vm.h>
+#include <swapfile.h>
 
-struct cm;
-struct cm_details;
+struct cm_details {
+    int index;
+    int vpn;                //virtual page num
+    int kern                //indicate the page is a kernel page
+    struct thread* program; //program owning page
+    struct cm_details* next_free;
+   
+};
+
+struct cm {
+    int size;
+    int clock_pointer;
+    int lowest_frame;
+    struct cm_details* core_details;
+    struct cm_details* free_frame_list = NULL;   
+};
 
 void cm_bootstrap();
 int cm_request_frame();
 void cm_release_frame(int frame_number);
 
+/// move the above to header file, ifndef block, blah blah blah
+////////////////////////////////////////////////
+
+struct cm core_map;
+
 void cm_bootstrap(){
+    assert(core_map == NULL); //we had better not bootstrap more than once!
+    
     core_map.size = mips_ramsize() / PAGE_SIZE;
     
     paddr_t physical_address = ram_stealmem(((sizeof(cm_details) * core_map.size) + PAGE_SIZE - 1) / PAGE_SIZE);
@@ -23,6 +45,7 @@ void cm_bootstrap(){
     paddr_t low;
     paddr_t high;
     ram_getsize(&low,&high);
+    assert(high != 0); //if this assertion fails, ram_getsize has been called before
     
     core_map.lowest_frame = (low / PAGE_SIZE);
     
@@ -36,57 +59,61 @@ void cm_bootstrap(){
         core_map.cm_details[i].program = NULL;
         core_map.cm_details[i].next_free = &(core_map.cm_details[i+1]);
     }
+    
     core_map.cm_details[core_map.size - 1].vpn = -1;
     core_map.cm_details[core_map.size - 1].kern = 0;
     core_map.cm_details[core_map.size - 1].program = NULL;
     core_map.cm_details[core_map.size - 1].next_free = NULL;
 }
 
-int cm_request_frame(){
-    if(core_map.free_frame_list == NULL){
-        //free list is empty, run page replacement algorithm
-        return cm_replace_page();
-    }else{
-        //get the frame from the free list
-        return cm_get_free_frame();
+void free_list_add(struct cm_details *new) {
+    if (core_map.free_frame_list == NULL) {
+        core_map.free_frame_list = new;
+        new->next_free = NULL;
+    } else {
+        new->next_free = core_map.free_frame_list;
+        core_map.free_frame_list = new;
     }
 }
 
-int cm_replace_page(){
-    //run the clock algorithm
-    while(true){
-        //TODO
+struct cm_details *free_list_pop() {
+    if (core_map.free_frame_list == NULL) {
+        return NULL;
     }
-}
-
-int cm_get_free_frame(){
-    int returnval = (int)(core_map.free_frame_list - core_map.core_details);
+    struct cm_details *retval;
+    retval = core_map.free_frame_list;
     core_map.free_frame_list = core_map.free_frame_list->next_free;
-    core_map.core_details[returnval].next_free = NULL;
-    return returnval;
+    return retval;
 }
 
-int cm_invalidate_frame_page(int frame_number){
-    //TODO
-    return 0;
+int cm_push_to_swap() {
+    for (int i = core_map.lowest_frame; i < core_map.size; i++) {
+        ///TODO: first pass of page replacement algorithm
+    }
+    for (int i = core_map.lowest_frame; i < core_map.size; i++) {
+        ///TODO: second of page replacement algorithm- should return before loop exits
+    }
+    //if we get here, then all pages are kernel pages (which must remain in RAM), so we're out of memory. That sucks!
+    panic("Too many kernel pages! No more space in RAM for a new page.");
 }
 
-struct cm core_map;
+int cm_request_frame(){
+    struct cm_details *frame = free_list_pop();
+    if (frame == NULL) {
+        /*
+        if no free pages are available, we need to push a frame into
+        swap to make room for a new frame in RAM
+        */
+        return cm_push_to_swap();
+    } else {
+        //there is a free page, so return it's index
+        return (((int) frame - (int) core_map.core_details) / sizeof(struct cm_details));
+    }
+}
 
-struct cm{
-    int size;
-    int clock_pointer;
-    int lowest_frame;
-    struct cm_details* core_details;
-    struct cm_details* free_frame_list = NULL;   
-};
+void cm_release_frame(int frame_number){
+    assert(frame_number >= core_map.lowest_frame);
+    free_list_add(&core_map.core_details[frame_number]);
+}
 
-struct cm_details{
-    int index;
-    int vpn;                //virtual page num
-    int kern                //indicate the page is a kernel page
-    struct thread* program; //program owning page
-    struct cm_details* next_free;
-   
-};
 #endif /* OPT_A3 */
